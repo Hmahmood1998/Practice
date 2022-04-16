f1eab7ca334b88a96131d816431c4b74505fc418
Hamid Mahamood <92150671+Hmahmood1998@users.noreply.github.com>
2022-04-16 23:37:49 +0530

    ...

--- main.py
+++ main.py
@@ -1,40 +1,17 @@
-from tracemalloc import Snapshot
 import streamlit as st
 import pandas as pd
-import numpy as np
-import matplotlib.pyplot as plt
-import seaborn as sns
 from visulization import *
-# import plotly.graph_objs as go
+import plotly.graph_objs as go
+
+
+st.title("Mall Customer Segmentation")
 
 def readData():
-    mall_customer = pd.read_csv('Mall_Customers.csv')
-    mall_customer.drop(["CustomerID"], axis=1, inplace=True)
-    return mall_customer
+    Mall_Customers = pd.read_csv('Mall_Customers.csv')
+    Mall_Customers.drop(["CustomerID"], axis=1, inplace=True)
+    return Mall_Customers
 
-df=readData()
-
-st.title("My Mini Project Name")
-
-plt.figure(1, figsize=(15,6))
-n=0
-for x in ['Age','Annual Income (k$)','Spending Score (1-100)']:
-    n+=1
-    plt.subplot(1,3,n)
-    plt.subplots_adjust(hspace=0.5, wspace=0.5)
-    sns.distplot(df[x], bins=20)
-    plt.title('Distplot of {}'.format(x))
-plt.show()
-
-plt.figure(1, figsize=(15,5))
-sns.countplot(y='Gender',data=df)
-plt.show()
-
-
-fig=plotBar(y='Gender',data=df)
-st.plotly_chart(fig, use_container_width=True)
-plt.show()
-
+df = readData()
 
 sidebar = st.sidebar
 
@@ -43,10 +20,8 @@ sidebar.title('User Options')
 
 def introduction():
     st.markdown("""
-        ## Heading Level 2
-        - Feature 1
-        - Feature 2
-        - Feature 3
+        ## Visulization of Mall Customer Segmentation
+        
     """)
 
     c1, c2 = st.columns(2)
@@ -57,8 +32,63 @@ def introduction():
 
 def execute():
     st.subheader('project working here')
+
     st.dataframe(df)
 
+    # genx=df[]
+    # fig=(data,)
+
+    # 1 Graph
+    st.markdown('## Numbers of Customer in different age gap ')
+    
+    data=df
+
+    age_18_25 = df.Age[(df.Age>=18) & (df.Age<=25)]
+    age_26_35 = df.Age[(df.Age>=26) & (df.Age<=35)]
+    age_36_45 = df.Age[(df.Age>=36) & (df.Age<=45)]
+    age_46_55 = df.Age[(df.Age>=46) & (df.Age<=55)]
+    age_55_above = df.Age[df.Age>=56]
+
+    agex = ["18-25","26-35","36-45","46-55","55+"]
+    agey = [len(age_18_25.values), len(age_26_35.values), len(age_36_45.values), len(age_46_55.values), len(age_55_above.values)]
+
+    fig=plotBar(data,agex,agey)
+    st.plotly_chart(fig,use_container_width=True)
+
+    st.markdown('## Numbers of Customer having the score ')
+
+    ss_1_20 = df["Spending Score (1-100)"][(df["Spending Score (1-100)"] >=1)&(df["Spending Score (1-100)"] <=20)]
+    ss_21_40 = df["Spending Score (1-100)"][(df["Spending Score (1-100)"] >=21)&(df["Spending Score (1-100)"] <=40)]
+    ss_41_60 = df["Spending Score (1-100)"][(df["Spending Score (1-100)"] >=41)&(df["Spending Score (1-100)"] <=60)]
+    ss_61_80 = df["Spending Score (1-100)"][(df["Spending Score (1-100)"] >=61)&(df["Spending Score (1-100)"] <=80)]
+    ss_81_100 = df["Spending Score (1-100)"][(df["Spending Score (1-100)"] >=81)&(df["Spending Score (1-100)"] <=100)]
+
+    ssx = ["1-20","21-40","41-60","61-80","81-100"]
+    ssy = [len(ss_1_20.values), len(ss_21_40.values), len(ss_41_60.values), len(ss_61_80.values), len(ss_81_100.values)]
+
+    fig=plotBar(data,ssx,ssy)
+    st.plotly_chart(fig,use_container_width=True)
+
+    st.markdown('## Scatter Chart between Anuual Income and Spending Score')
+
+    fig=plotScatter(data,'Annual Income (k$)','Spending Score (1-100)')
+    st.plotly_chart(fig,use_container_width=True)
+
+    st.markdown('## Numbers Of Customer having different Annual Income gap')
+
+    ai_0_30 = df["Annual Income (k$)"][(df["Annual Income (k$)"] >=0)&(df["Annual Income (k$)"] <=30)]
+    ai_31_60 = df["Annual Income (k$)"][(df["Annual Income (k$)"] >=21)&(df["Annual Income (k$)"] <=60)]
+    ai_61_90 = df["Annual Income (k$)"][(df["Annual Income (k$)"] >=61)&(df["Annual Income (k$)"] <=90)]
+    ai_91_120 = df["Annual Income (k$)"][(df["Annual Income (k$)"] >=91)&(df["Annual Income (k$)"] <=120)]
+    ai_121_150 = df["Annual Income (k$)"][(df["Annual Income (k$)"] >=121)&(df["Annual Income (k$)"] <=150)]
+
+    ssx = ["$ 0-30,000","$ 30,001-60,000","$ 60,001-90,000","$ 90,001-1,20,000","$ 1,20,000-1,50,000"]
+    ssy = [len(ai_0_30.values), len(ai_31_60.values), len(ai_61_90.values), len(ai_91_120.values), len(ai_121_150.values)]
+
+    fig=plotBar(data,ssx,ssy)
+    st.plotly_chart(fig,use_container_width=True)
+    
+    
 
 options = ['Project Introduction', 'Execution']
 
--- visulization.py
+++ visulization.py
@@ -1,6 +1,15 @@
 from os import linesep
 import plotly.express as px
 
-def plotBar(data,y):
-    return px.bar(data_frame=data,y=y)
 
+def plotBar(data,x,y):
+    return px.bar(data_frame=data,x=x,y=y)
+
+def plotLine(data,x,y):
+    return px.line(data_frame=data,x=x,y=y)
+
+def plotPie(data,labels):
+    return px.pie(data_frame=data,labels=labels)
+
+def plotScatter(data,x,y):
+    return px.scatter(data_frame=data,x=x,y=y)
